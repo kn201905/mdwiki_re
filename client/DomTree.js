d26de0098e481257429b6cb472caf5ff1bca1a28
kn201905 <zz2018-dr@yahoo.co.jp>
2021-01-06 14:43:22 +0900

    Update DomTree.js

--- client/DomTree.js
+++ client/DomTree.js
@@ -1,39 +1,42 @@
 'use strict';
 
 const g_DomTree = new function() {
-	let m_e_div_dst = null;
-
 	let m_cur_div;
-	let m_Quote_div;
 
-	let mb_CodeBlk;
-	let mb_Quote;
-
+	let mb_QuoteBlk;
 	let mb_idx_DivBlk;
 
-	// div_dst は、g_e_DomTreeArea を想定している
-	// buf_reader は、g_Read_Buf を想定している
-	// ID_Lexed_MD を検知した後、コールされることを想定している
-	this.BuildTree = (e_div_dst) => {
-		e_div_dst.Remove_All();
-		m_e_div_dst = e_div_dst;
+	let e_oframe_div_creating;
+	
+	// ID_Lexed_MD を検知した後の、g_TStream_Reader を用いて dom_tree を生成する
+	this.BuildTree = () => {
+		e_oframe_div_creating = document.createElement('div');;
+		
+		e_oframe_div_creating.classList.add('DomTree');
+		{
+			const e_top_dom_tree = e_oframe_div_creating.Add_Div();
+			e_top_dom_tree.classList.add('HLine');
+			e_top_dom_tree.style.marginBottom = '1rem';
+		}
 
 		m_cur_div = null;
-		m_Quote_div = null;
-
-		mb_CodeBlk = false;
-		mb_Quote = false;
+		mb_QuoteBlk = false;
 
 		mb_idx_DivBlk = 0;
 
-		g_Read_Buf.Reset_idx();
 		while (true) {
-			const id = g_Read_Buf.Consume_NextID();
+			const id = g_TStream_Reader.Consume_NextID();
 			let e_err_div;  // エラー用の Div として利用する
 
 			if (id & ID_Text)
 			{
-				m_cur_div.appendChild(document.createTextNode(g_Read_Buf.Get_text_cur()));
+				const e_new = m_cur_div.Add_Element(null);
+				e_new.textContent = g_TStream_Reader.Get_text_cur();
+
+				if (id & TxtFLG_Bold) { e_new.style.fontWeight = 'bold'; }
+				if (id & TxtFLG_Cancel) { e_new.style.textDecoration = 'line-through'; }
+				if (id & TxtFLG_Under) { e_new.classList.add('inline_Underline'); }
+				if (id & TxtFLG_Code) { e_new.classList.add('inline_Code'); }
 				continue;
 			}
 
@@ -45,7 +48,7 @@ const g_DomTree = new function() {
 				}
 				
 				console.log("+++ ERR -> idx_DivBlk : " + mb_idx_DivBlk.toString());
-				return;  // ここで BuildTree() を打ち切る
+				return e_oframe_div_creating;  // ここで BuildTree() を打ち切る
 			}
 
 			switch (id)
@@ -55,49 +58,73 @@ const g_DomTree = new function() {
 				continue;
 
 			case ID_End:
-				return;  // BuildTree() の正常終了
+				return e_oframe_div_creating;  // BuildTree() の正常終了
 
 			case ID_BR:
 				m_cur_div.Add_BR();
 				continue;
 
+			case ID_HLine:
+				const e_HLine = e_oframe_div_creating.Add_Div();
+				e_HLine.classList.add('HLine');
+				m_cur_div = null;  // エラー顕在化
+				continue;
+
 			// -------------------------------------------------------------------
 			case ID_ERR_OVERFLOW:
-				e_err_div = m_e_div_dst.Add_DivTxt("!!! ERR_OVERFLOW を検出しました。DomTree の生成を中断します。");
+				e_err_div = e_oframe_div_creating.Add_DivTxt("!!! ERR_OVERFLOW : DomTree の生成を中断します。");
 				break;
 
 			case ID_ERR_Report:
-				e_err_div = m_e_div_dst.Add_DivTxt("!!! ERR_Report を検出しました。");
+				e_err_div = e_oframe_div_creating.Add_DivTxt("!!! ERR_Report : DomTree の生成を中断します。");
 				break;
 
 			case ID_ERR_on_Simplify:
-				e_err_div = m_e_div_dst.Add_DivTxt("!!! ERR_on_Simplify を検出しました。");
+				e_err_div = e_oframe_div_creating.Add_DivTxt("!!! ERR_on_Simplify : DomTree の生成を中断します。");
 				break;
+				
+			default:
+				e_err_div = e_oframe_div_creating.Add_DivTxt("!!! 不明な ID を検出 : id -> " + id);
+				e_err_div.style.fontSize = '2rem';
+				e_err_div.style.color = '#F00';
+				return e_oframe_div_creating;  // id が不明なため、dom_tree の生成を abort する。
 			}
 
 			// -------------------------------------------------------------------
-			// エラー処理
-			// TODO: e_err_div を、エラー用にすること
-		}
-	};
-
+			// エラー表示処理
+			console.log(e_err_div);
+			
+			e_err_div.style.fontSize = '2rem';
+			e_err_div.style.color = '#F00';
+			
+			// QuoteBlk に入っていたら、それを抜け出す
+			if (mb_QuoteBlk == true) {
+				const e_QuoteBlk = e_oframe_div_creating.parentNode;
+				e_oframe_div_creating = e_QuoteBlk.parentNode;
+				m_cur_div = null;  // バグ検出用
+			}
+		} // while
+	}; // BuildTree
+	
+	// -------------------------------------------------------------------------
 	const Build_DivBlk = (id) => {
 		switch(id)
 		{
 		case ID_Div:
-			m_cur_div = m_e_div_dst.Add_Div();
+			m_cur_div = e_oframe_div_creating.Add_Div();
 //			m_cur_div.classList.add('Div');
 			break;
 
 		case ID_Div_Head:
-			// TODO: Head にすること
-			m_cur_div = m_e_div_dst.Add_Div();
+			m_cur_div = e_oframe_div_creating.Add_Div();
 			m_cur_div.classList.add('Head');
-			const heed_num = g_Read_Buf.Get_param_cur();
+			const heed_num = g_TStream_Reader.Get_param_cur();
 			switch (heed_num)
 			{
 			case 1:
-				m_cur_div.style.fontSize = '2rem';
+				m_cur_div.style.fontSize = '1.8rem';
+//				const e_h1_underline = e_oframe_div_creating.Add_Div();
+//				e_h1_underline.classList.add('h1_underline');
 				break;
 			
 			case 2:
@@ -107,50 +134,55 @@ const g_DomTree = new function() {
 			break;
 
 		case ID_Div_Code:
-			mb_CodeBlk = !mb_CodeBlk;
-			// mb_CodeBlk == false の場合は、処理が不要なはず、、、
-			if (mb_CodeBlk == true)
-			{
-				// TODO: CodeBlk にすること
-				m_cur_div = m_e_div_dst.Add_Div();
-				m_cur_div.classList.add('CodeBlk');
-			}
+			m_cur_div = e_oframe_div_creating.Add_Div();
+			m_cur_div.classList.add('CodeBlk');
 			break;
 
 		case ID_Div_Quote:
-			mb_Quote = !mb_Quote;
-			if (mb_Quote == true) {
+			mb_QuoteBlk = !mb_QuoteBlk;
+			if (mb_QuoteBlk == true) {
 				// Quote ブロックに入る
-				const e_QuoteBlk = m_e_div_dst.Add_FlexStg();
+				const e_QuoteBlk = e_oframe_div_creating.Add_FlexStg();
 				e_QuoteBlk.classList.add('QuoteBlk');
 
 				const e_left_stg = e_QuoteBlk.Add_Div();
-				e_left_stg.classList.add('QuoteLine');
+				e_left_stg.classList.add('Quote_leftLine');
 
-				// m_e_div_dst は right_stg となる
+				// e_oframe_div_creating は right_stg となる
 				const e_right_stg = e_QuoteBlk.Add_Div();
-				m_e_div_dst = e_right_stg;  // m_e_div_dst をリカーシブに利用
+				e_oframe_div_creating = e_right_stg;  // e_oframe_div_creating をリカーシブに利用
 				m_cur_div = null;  // バグ検出用
 			}
 			else {
 				// Quote ブロックから出る
-				const e_QuoteBlk = m_e_div_dst.parentNode;
-				m_e_div_dst = e_QuoteBlk.parentNode;
+				const e_QuoteBlk = e_oframe_div_creating.parentNode;
+				e_oframe_div_creating = e_QuoteBlk.parentNode;
 				m_cur_div = null;  // バグ検出用
 			}
 			break;
 
+		case ID_Div_Bullet:
+			const e_BulletBlk = e_oframe_div_creating.Add_Div();
+			e_BulletBlk.style.display = 'flex';
+			e_BulletBlk.style.marginTop = '1rem';
+			
+			const e_left_stg = e_BulletBlk.Add_Div();
+			e_left_stg.textContent = "\xa0•\xa0";
+
+			m_cur_div = e_BulletBlk.Add_Div();  // right_stg
+			return true;  // break すると、Div の　BR_above 処理が入ってしまうため注意
+
 		default:
 			// TODO: エラー用にすること
-			const e_err_div = m_e_div_dst.Add_DivTxt("!!! 不明な ID を検出しました。ここで DomTree の生成を中断します。 id : " + id.toString());
+			const e_err_div = e_oframe_div_creating.Add_DivTxt("!!! 不明な ID を検出。ここで DomTree の生成を中断します。 id : " + id.toString());
 			return false;
 		} // switch
 
 		// Div の　BR_above 処理
-		if (m_cur_div && (g_Read_Buf.Get_param_cur() & Param_no_BR_above) == 0) {
+		if (m_cur_div && g_TStream_Reader.FLG_no_BR_above() == 0) {
 			m_cur_div.style.marginTop = '1rem';
 		}
 
 		return true;
-	}
+	} // Build_DivBlk
 };
